c2c087f2e7c95b4741ea638ee538af40cd261818
Kolawole Paul Oluwafemi <k.paul@Kolawoles-MacBook-Pro.local>
2024-01-19 11:51:26 -0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bot_toast/bot_toast.dart';
+import 'package:flutter/material.dart';
+import 'package:lights_on_height_ecommerce_app/app/views/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final botToastBuilder = BotToastInit();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '',
+      builder: botToastBuilder,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
