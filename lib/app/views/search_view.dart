76ca946f84373850780ca3c4f627c75bc721725e
Kolawole Paul Oluwafemi <k.paul@Kolawoles-MacBook-Pro.local>
2024-01-20 01:34:11 -0800

    search implementation commit

--- lib/app/views/search_view.dart
+++ lib/app/views/search_view.dart
@@ -91,34 +91,32 @@ class _SearchViewState extends State<SearchView> {
       backgroundColor: Colors.grey.shade100,
       appBar: _buildAppBar(),
       body: isLoadingProducts
-          ? Expanded(
-              child: ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: 8,
-                padding: const EdgeInsets.only(
-                  bottom: 10.0,
-                  left: 18.0,
-                  right: 18.0,
-                  top: 12.0,
+          ? ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: 8,
+            padding: const EdgeInsets.only(
+              bottom: 10.0,
+              left: 18.0,
+              right: 18.0,
+              top: 12.0,
+            ),
+            itemBuilder: (context, index) {
+              return Shimmer.fromColors(
+                baseColor: Colors.grey.withOpacity(.1),
+                highlightColor: Colors.white60,
+                child: Container(
+                  height: 76,
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(top: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(.7),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
                 ),
-                itemBuilder: (context, index) {
-                  return Shimmer.fromColors(
-                    baseColor: Colors.grey.withOpacity(.1),
-                    highlightColor: Colors.white60,
-                    child: Container(
-                      height: 76,
-                      width: MediaQuery.of(context).size.width,
-                      margin: const EdgeInsets.only(top: 8),
-                      decoration: BoxDecoration(
-                        color: Colors.grey.withOpacity(.7),
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            )
+              );
+            },
+          )
           : searchProductsResultList.isEmpty &&
                   _searchTextController.text.isNotEmpty
               ? const Center(
